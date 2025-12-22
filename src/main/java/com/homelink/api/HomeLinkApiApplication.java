eda49c464add6122ed434bcbbcddee2f628fe415
Chantha makara <chantha.makara@institute.pse.ngo>
2025-12-22 10:06:04 +0700

    first commit

---
+++ src/main/java/com/homelink/api/HomeLinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.homelink.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLinkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homelink/api/HomeLinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homelink.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

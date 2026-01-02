b1baa5faf132714e0f70cea454ede1317d553e0f
Chantha makara <chantha.makara@institute.pse.ngo>
2026-01-02 13:17:06 +0700

    merge develop to staging

--- src/main/java/com/homelink/api/entity/Review.java
+++ src/main/java/com/homelink/api/entity/Review.java
@@ -24,6 +24,7 @@ public class Review {
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+    
 
     private LocalDateTime createdAt = LocalDateTime.now();
 }
